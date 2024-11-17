3ef2fecf48f1f17faf0cf6932183e3af311df122
lakshmiPrasanna-chebrolu <chebrolu.lakshmi.prasanna.lp.147@gmail.com>
2024-11-17 17:52:54 +0530

    [Api Controllers] - Created the database migrations using the EF core and user controllers.

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace API.Entities;// should be physical folder location
+//EntiryFrameWork uses only properties having the public access modifiers.
+//class represents table and properties represent the column
+
+public class AppUser
+{
+    public int Id { get; set; }
+    public required string UserName { get; set; }
+
+}
