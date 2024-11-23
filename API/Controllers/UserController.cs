a4c0856020e379d31ec88694e62bdb54eeda223e
lakshmiPrasanna-chebrolu <chebrolu.lakshmi.prasanna.lp.147@gmail.com>
2024-11-24 04:20:24 +0530

    Implemented the Login and Register APIs along with Authentication and JWT tokens

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -1,22 +1,24 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-
-    public class UsersController(DataContext context) : ControllerBase
+[Authorize]
+    public class UsersController(DataContext context) : BaseApiController
     {
+        [AllowAnonymous]
          [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
             var users= await context.Users.ToListAsync();
             return  Ok(users);
         }
 
+
+        [Authorize]
         [HttpGet("{id:int}")]
         public async Task<ActionResult<AppUser>> GetUser(int id ){
             var user= await context.Users.FindAsync(id);
