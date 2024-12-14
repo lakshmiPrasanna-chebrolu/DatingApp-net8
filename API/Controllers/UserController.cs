b62eda9c5d90a3c021333bfaba9f66402c9373c4
lakshmiPrasanna-chebrolu <chebrolu.lakshmi.prasanna.lp.147@gmail.com>
2024-12-15 01:32:12 +0530

    Caching data in angular and usage of CanDeactivate route guard

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -10,29 +11,46 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-[Authorize]
-    public class UsersController(IUserRepository userRepository,IMapper mapper) : BaseApiController
+    [Authorize]
+    public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
     {
-         [HttpGet]
-        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers(){
-            var users= await userRepository.GetMembersAsync();
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+        {
+            var users = await userRepository.GetMembersAsync();
             //var usersToReturn=mapper.Map<IEnumerable<MemberDto>>(users);
-            return  Ok(users);
+            return Ok(users);
         }
 
 
         [HttpGet("{id:int}")]
-        public async Task<ActionResult<MemberDto>> GetUserById(int id ){
-            var user= await userRepository.GetUserByIdAsync(id);
-            if(user==null) return NotFound();
-            return  mapper.Map<MemberDto>(user);
+        public async Task<ActionResult<MemberDto>> GetUserById(int id)
+        {
+            var user = await userRepository.GetUserByIdAsync(id);
+            if (user == null) return NotFound();
+            return mapper.Map<MemberDto>(user);
         }
 
         [HttpGet("{username}")]
-        public async Task<ActionResult<MemberDto>> GetUser(string username ){
-            var user= await userRepository.GetMemberByUserNameAsync(username);
-            if(user==null) return NotFound();
-            return  user;//mapper.Map<MemberDto>(user);
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
+        {
+            var user = await userRepository.GetMemberByUserNameAsync(username);
+            if (user == null) return NotFound();
+            return user;//mapper.Map<MemberDto>(user);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username=User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if(username==null) return BadRequest("No username found in token");
+            var user= await userRepository.GetUserByUserNameAsync(username);
+            if(user==null) return BadRequest("Could not find the user");
+            mapper.Map(memberUpdateDto,user);
+            //userRepository.Update(user);
+            if(await userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update the user");
         }
     }
 }
